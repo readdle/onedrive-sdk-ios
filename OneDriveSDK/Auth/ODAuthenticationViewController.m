b24487f204698f2d8eca1a5bb4a495434710179a
Savkov Alexander <asavkov@readdle.com>
2020-07-09 17:43:32 +0300

    Dev: drop UIWebView

--- OneDriveSDK/Auth/ODAuthenticationViewController.m
+++ OneDriveSDK/Auth/ODAuthenticationViewController.m
@@ -26,9 +26,7 @@
 
 #define kRequestTimeoutDefault  60
 
-@interface ODAuthenticationViewController() <UIWebViewDelegate>
-
-@property UIWebView *webView;
+@interface ODAuthenticationViewController()
 
 @property NSURLRequest *initialRequest;
 @property (strong, nonatomic) ODEndURLCompletion successCompletion;
@@ -73,7 +71,7 @@
 
 - (void)loadInitialRequest
 {
-    [self.webView loadRequest:self.initialRequest];
+    
 }
 
 - (void)redirectWithStartURL:(NSURL *)startURL
@@ -84,96 +82,15 @@
     self.successCompletion = successCompletion;
     self.initialRequest = [NSURLRequest requestWithURL:startURL];
     self.isComplete = NO;
-    [self.webView loadRequest:self.initialRequest];
 }
 
 - (void)loadView
 {
-    self.webView = [[UIWebView alloc] init];
-    [self.webView setScalesPageToFit:YES];
-    self.webView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    self.webView.delegate = self;
-    self.view = self.webView;
     UIBarButtonItem *cancel = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel
                                                                                 target:self
                                                                                 action:@selector(cancel)];
     self.navigationController.topViewController.navigationItem.leftBarButtonItem = cancel;
     
-}
-
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-    [self.webView loadRequest:self.initialRequest];
-}
-
-- (void)viewWillDisappear:(BOOL)animated
-{
-    [self.webView stopLoading];
-    self.webView.delegate = nil;
-    [super viewWillDisappear:animated];
-}
-
-#pragma mark - UIWebViewDelegate
-
-- (void)webViewDidStartLoad:(UIWebView *)webView
-{
-    [self.timer invalidate];
-    self.timer = [NSTimer scheduledTimerWithTimeInterval:self.requestTimeout target:self selector:@selector(failWithTimeout) userInfo:nil repeats:NO];
-}
-
-- (void)webViewDidFinishLoad:(UIWebView *)webView
-{
-    [self.timer invalidate];
-    self.timer = nil;
-}
-
-- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
-{
-    if ([[[request.URL absoluteString] lowercaseString] hasPrefix:[[self.endURL absoluteString] lowercaseString]]){
-        self.isComplete = YES;
-        [self.timer invalidate];
-        self.timer = nil;
-        
-        self.successCompletion(request.URL, nil);
-        return NO;
-    }
-    return YES;
-}
-
-- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
-{
-    [self.timer invalidate];
-    self.timer = nil;
-    
-    if (NSURLErrorCancelled == error.code)
-    {
-        //This is a common error that webview generates and could be ignored.
-        //See this thread for details: https://discussions.apple.com/thread/1727260
-        return;
-    }
-    
-    if([error.domain isEqual:@"WebKitErrorDomain"]){
-        return;
-    }
-    
-    // Ignore failures that are triggered after we have found the end URL
-    if (self.isComplete)
-    {
-        //We expect to get an error here, as we intentionally fail to navigate to the final redirect URL.
-        return;
-    }
-    
-    if (self.successCompletion) {
-        self.successCompletion(nil, error);
-    }
-}
-
-- (void)failWithTimeout
-{
-    [self webView:self.webView didFailLoadWithError:[NSError errorWithDomain:NSURLErrorDomain
-                                                                    code:NSURLErrorTimedOut
-                                                                userInfo:nil]];
 }
 
 @end
